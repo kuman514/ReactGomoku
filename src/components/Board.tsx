84d9b0259fec147ca42808e85f82c6c2f69feb03
kumankoishi.py <7iggif@naver.com>
2021-10-10 10:52:41 +0900

    Implementing Board set stone

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,18 +2,59 @@ import React, { useState } from 'react';
 import BoardButton from './BoardButton';
 
 function Board() {
-  const [tiles, setTiles] = useState(
-    Array.from({length: 19}, () => Array.from({length: 19}, () => 0))
-  );
+  const [status, setStatus] = useState({
+    tiles: Array.from({length: 19}, () => Array.from({length: 19}, () => 0)),
+    curPlayer: 1
+  });
+
+  const putStone = (row: number, col: number) => {
+    if (status.tiles[row][col] !== 0) {
+      return;
+    }
+
+    const newTiles = Array.from({length: 19}, (_, i) => Array.from(status.tiles[i]));
+    let newPlayer = 0;
+
+    newTiles[row][col] = status.curPlayer;
+    switch (status.curPlayer) {
+      case 1:
+        newPlayer = 2;
+        break;
+      case 2:
+        newPlayer = 1;
+        break;
+    }
+
+    setStatus({
+      tiles: newTiles,
+      curPlayer: newPlayer
+    });
+  };
+
+  /*
+  const resetBoard = () => {
+    setStatus({
+      tiles: Array.from({length: 19}, () => Array.from({length: 19}, () => 0)),
+      curPlayer: 1
+    });
+  }
+  */
 
   return (
     <div className="Board" onClick={(event) => {
-      console.log((event.target as HTMLElement).id);
+      //console.log((event.target as HTMLElement).id);
+      const [row, col] = (event.target as HTMLElement).id.split(',').map((item) => parseInt(item));
+      //console.log(row, col);
+      if (Number.isNaN(row) || Number.isNaN(col)) {
+        return;
+      }
+
+      putStone(row, col);
     }}>
       {
-        tiles.map((line, i) => {
+        status.tiles.map((line, i) => {
           return line.map((item, j) => {
-            return (<BoardButton keyPos={`r${i}c${j}`} who={item} clicked={item !== 0} />);
+            return (<BoardButton key={`r${i}c${j}`} keyPos={`${i},${j}`} who={item} clicked={item !== 0} />);
           });
         })
       }
