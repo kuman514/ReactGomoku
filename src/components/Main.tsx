4c98006f536e95ea8eb599f10640e9ff6e9beb26
kumankoishi.py <7iggif@naver.com>
2021-10-17 00:06:49 +0900

    Added SFX props

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,10 +3,24 @@ import Title from './Title';
 import Board from './Board';
 
 function Main() {
+  const SFXs = {
+    player1PutSFX: document.querySelector('#player1PutSFX'),
+    player2PutSFX: document.querySelector('#player2PutSFX'),
+    resetSFX: document.querySelector('#resetSFX'),
+    undoSFX: document.querySelector('#undoSFX'),
+    resultSFX: document.querySelector('#resultSFX')
+  };
+
   return (
     <main className="Main">
       <Title title="React Gomoku" />
-      <Board />
+      <Board
+        player1PutSFX={SFXs.player1PutSFX as HTMLAudioElement}
+        player2PutSFX={SFXs.player2PutSFX as HTMLAudioElement}
+        resetSFX={SFXs.resetSFX as HTMLAudioElement}
+        undoSFX={SFXs.undoSFX as HTMLAudioElement}
+        resultSFX={SFXs.resultSFX as HTMLAudioElement}
+      />
     </main>
   );
 }
