389d8987397b202dcdbbe6e203c8402dd6e1daa0
kumankoishi.py <7iggif@naver.com>
2021-12-06 10:29:33 +0900

    Updated WinnerEffect

--- src/components/WinnerEffect.tsx
+++ src/components/WinnerEffect.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { initState, StoreState } from '../store/StoreState';
 
-interface WinnerEffectProps {
-  winner: number
-}
 
-function WinnerEffect(props: WinnerEffectProps) {
+interface WinnerEffectState {
+  winner: number,
+  mode: string
+};
+
+function WinnerEffect() {
+  const selector = (state: StoreState = initState): WinnerEffectState => {
+    return {
+      winner: state.winner,
+      mode: state.mode
+    }
+  };
+  const status = useSelector(selector);
+  const renderWinnerEffect = (): JSX.Element => {
+    if (status.mode === 'GAME' && status.winner !== 0) {
+      return (
+        <div className="WinnerEffect">
+          <div className="WinnerEffectContent">
+            <span>
+              Player { status.winner } Wins!
+            </span>
+          </div>
+        </div>
+      );
+    } else {
+      return (<div></div>);
+    }
+  };
+
   return (
-    <div className="WinnerEffect">
-      <div className="WinnerEffectContent">
-        <span>
-          Player { props.winner } Wins!
-        </span>
-      </div>
-    </div>
+    renderWinnerEffect()
   );
 }
 
