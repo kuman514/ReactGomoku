9484529dd3aec3b01ca70c7227a811e9e0462738
kumankoishi.py <7iggif@naver.com>
2021-12-07 09:51:35 +0900

    Detached theme from main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,44 +4,14 @@ import Board from './Board';
 import Bottom from './Bottom';
 import ModeChanger from './ModeChanger';
 import WinnerEffect from './WinnerEffect';
-
-const THEME_BUTTONS = {
-  'default': ['⚫', '⚪'],
-  'halloween': ['🟤', '🟠'],
-  'winter': ['🔴', '🟢']
-};
-
-const THEME_TITLES = {
-  'default': 'React Gomoku',
-  'halloween': 'Halloween Gomoku',
-  'winter': 'Holiday Gomoku'
-};
-
-const THEME_INDEX = (() => {
-  const dat: Date = new Date();
-
-  const day: number = dat.getDate();
-  const month: number = dat.getMonth();
-
-  if (month === 9 && day === 31) {
-    // October 31
-    return 'halloween';
-  } else if ((month === 0 && day === 1) || (month === 11 && day === 25)) {
-    // January 1 or December 25
-    return 'winter';
-  }
-
-  return 'default';
-})();
+import { themeTitle } from '../theme/Theme';
 
 function Main() {
   return (
     <main className="Main">
-      <Title title={THEME_TITLES[THEME_INDEX]} />
+      <Title title={themeTitle} />
       <ModeChanger />
-      <Board
-        themeButtons={THEME_BUTTONS[THEME_INDEX]}
-      />
+      <Board />
       <Bottom />
       <WinnerEffect />
     </main>
---
+++ src/theme/Theme.tsx
@@ -0,0 +1,31 @@
+const THEME_BUTTONS = {
+  'default': ['⚫', '⚪'],
+  'halloween': ['🟣', '🟠'],
+  'winter': ['🔴', '🟢']
+};
+
+const THEME_TITLES = {
+  'default': 'React Gomoku',
+  'halloween': 'Halloween Gomoku',
+  'winter': 'Holiday Gomoku'
+};
+
+const THEME_INDEX = (() => {
+  const dat: Date = new Date();
+
+  const day: number = dat.getDate();
+  const month: number = dat.getMonth();
+
+  if (month === 9 && day === 31) {
+    // October 31
+    return 'halloween';
+  } else if ((month === 0 && day === 1) || (month === 11 && day === 25)) {
+    // January 1 or December 25
+    return 'winter';
+  }
+
+  return 'default';
+})();
+
+export const themeButtons: string[] = THEME_BUTTONS[THEME_INDEX];
+export const themeTitle: string = THEME_TITLES[THEME_INDEX];
