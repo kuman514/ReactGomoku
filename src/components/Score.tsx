f465a04ba5f19a90fbd4579491ea9a2ba8d41010
kumankoishi.py <7iggif@naver.com>
2021-12-16 16:22:00 +0900

    Added score component

---
+++ src/components/Score.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { initState, StoreState } from '../store/StoreState';
+
+interface ScoreState {
+  p1Score: number,
+  p2Score: number
+}
+
+function Score() {
+  const selector = (state: StoreState = initState): ScoreState => {
+    return {
+      p1Score: state.score[0],
+      p2Score: state.score[1]
+    };
+  };
+  const status = useSelector(selector);
+
+  useEffect(() => {
+    document.title = `React Gomoku :: ${status.p1Score} - ${status.p2Score}`;
+  });
+
+  return (
+    <div className="Score">
+      <span>
+        {status.p1Score} - {status.p2Score}
+      </span>
+    </div>
+  );
+}
+
+export default Score;
