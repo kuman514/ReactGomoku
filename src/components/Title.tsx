5f11c723529f7669bdfa3d33ff8c3f39f03eb182
kumankoishi.py <7iggif@naver.com>
2022-05-31 22:36:15 +0900

    Styled-componented Title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface TitleProps {
   title: string
 }
 
+const TitleElement = styled.div`
+  margin-right: 1vw;
+  & * {
+    display: inline;
+  }
+`;
+
 function Title(props: TitleProps) {
   return (
-    <div className="Title">
+    <TitleElement>
       { props.title }
-    </div>
+    </TitleElement>
   );
 }
 
