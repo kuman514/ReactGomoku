4b5faf105c2fc1e4dfa75ae64c0fa2883a5b19f6
kumankoishi.py <7iggif@naver.com>
2022-06-01 07:03:30 +0900

    Styled-componented WinnerEffect

--- src/components/WinnerEffect.tsx
+++ src/components/WinnerEffect.tsx
@@ -1,12 +1,59 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 import { initState, StoreState } from '../store/StoreState';
-
 
 interface WinnerEffectState {
   winner: number,
   mode: string
 };
+
+const WinnerEffectElement = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+  pointer-events: none;
+  animation: WinnerEffectAnimation linear 3000ms;
+  animation-iteration-count: 1;
+
+  @keyframes WinnerEffectAnimation {
+    0% {
+      opacity: 1;
+    }
+    70% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+`;
+
+const WinnerEffectContentElement = styled.div`
+  width: inherit;
+  height: inherit;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-weight: bolder;
+  animation: WinnerEffectContentAnimation linear 3000ms;
+  animation-iteration-count: 1;
+
+  @keyframes WinnerEffectContentAnimation {
+    0% {
+      font-size: 0;
+    }
+    10% {
+      text-shadow: 0 0 5vw #61dafb, 0 0 3vw #61dafb, 0 0 1vw #61dafb;
+      font-size: 250%;
+    }
+  }
+`;
 
 function WinnerEffect() {
   const selector = (state: StoreState = initState): WinnerEffectState => {
@@ -16,25 +63,23 @@ function WinnerEffect() {
     }
   };
   const status = useSelector(selector);
-  const renderWinnerEffect = (): JSX.Element => {
+  const renderWinnerEffect = () => {
     if (status.mode === 'GAME' && status.winner !== 0) {
       return (
-        <div className="WinnerEffect">
-          <div className="WinnerEffectContent">
+        <WinnerEffectElement>
+          <WinnerEffectContentElement>
             <span>
               Player { status.winner } Wins!
             </span>
-          </div>
-        </div>
+          </WinnerEffectContentElement>
+        </WinnerEffectElement>
       );
     } else {
-      return (<div></div>);
+      return null;
     }
   };
 
-  return (
-    renderWinnerEffect()
-  );
+  return renderWinnerEffect();
 }
 
 export default WinnerEffect;
