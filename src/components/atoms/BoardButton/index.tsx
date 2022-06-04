ee4128d42b079578995826d73b4127c8a8e7c4e7
kumankoishi.py <7iggif@naver.com>
2022-06-05 01:26:24 +0900

    Added atomic BoardButton

---
+++ src/components/atoms/BoardButton/index.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+import LeftTopCorner from 'Img/LeftTopCorner.png';
+import TopEdge from 'Img/TopEdge.png';
+import RightTopCorner from 'Img/RightTopCorner.png';
+import LeftEdge from 'Img/LeftEdge.png';
+import InBoard from 'Img/InBoard.png';
+import RightEdge from 'Img/RightEdge.png';
+import LeftBottomCorner from 'Img/LeftBottomCorner.png';
+import BottomEdge from 'Img/BottomEdge.png';
+import RightBottomCorner from 'Img/RightBottomCorner.png';
+
+import * as T from 'types';
+
+interface BoardButtonStyleProps {
+  position: T.ButtonPosition;
+  color: string;
+  flash?: boolean;
+}
+
+export const urls: T.ButtonBackgroundUrls = {
+  [T.ButtonPosition.LeftTopCorner]: LeftTopCorner,
+  [T.ButtonPosition.TopEdge]: TopEdge,
+  [T.ButtonPosition.RightTopCorner]: RightTopCorner,
+  [T.ButtonPosition.LeftEdge]: LeftEdge,
+  [T.ButtonPosition.InBoard]: InBoard,
+  [T.ButtonPosition.RightEdge]: RightEdge,
+  [T.ButtonPosition.LeftBottomCorner]: LeftBottomCorner,
+  [T.ButtonPosition.BottomEdge]: BottomEdge,
+  [T.ButtonPosition.RightBottomCorner]: RightBottomCorner,
+};
+
+const BoardButton = styled.button`
+  all: unset;
+  box-sizing: border-box;
+  margin: 0;
+  background-size: 100%;
+  color: ${(props: BoardButtonStyleProps) => props.color};
+  background-image: url(${(props: BoardButtonStyleProps) => {
+    return urls[props.position];
+  }});
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.5;
+  }
+
+  &:focus {
+    border: 3px solid red;
+  }
+
+  animation: ${(props: BoardButtonStyleProps) => props.flash
+    ? 'FlashTrackedResult linear infinite 600ms'
+    : 'none'
+  };
+  @keyframes FlashTrackedResult {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0.3;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+export default BoardButton;
