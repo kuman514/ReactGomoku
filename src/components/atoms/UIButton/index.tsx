34a9e1339ab747fb5899d5a54e54f8398bdbbdb0
kumankoishi <7iggif@naver.com>
2022-10-13 23:32:32 +0900

    refactor(ui-button): refactor ui button

--- src/components/atoms/UIButton/index.tsx
+++ src/components/atoms/UIButton/index.tsx
@@ -11,22 +11,22 @@ const UIButton = styled.button`
   margin: 0;
   box-sizing: border-box;
   background: transparent;
-  width: ${(props: UIButtonProps) => props.width ?? 'auto'};
+  width: ${({ width }: UIButtonProps) => width ?? 'auto'};
   height: calc(10px + 2vmin + 0.8vh);
   font-size: calc(8px + 2vmin);
   padding-left: 0.25vw;
   padding-right: 0.25vw;
   padding-top: 0.25vh;
   padding-bottom: 0.25vh;
-  color: ${(props: UIButtonProps) => props.color ?? '#61dafb'};
-  border: 1px solid ${(props: UIButtonProps) => props.color ?? '#61dafb'};
+  color: ${({ color }: UIButtonProps) => color ?? '#61dafb'};
+  border: 1px solid ${({ color }: UIButtonProps) => color ?? '#61dafb'};
   border-radius: 10px;
   transition: all linear 120ms;
 
   &:hover {
     cursor: pointer;
     color: #ffffff;
-    background-color: ${(props: UIButtonProps) => props.color ?? '#61dafb'};
+    background-color: ${({ color }: UIButtonProps) => color ?? '#61dafb'};
   }
 
   &:disabled {
